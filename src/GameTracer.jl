0286eba727a49378d1522794d44534eb84b633ae
mnshkw <mao.nishikawa24@gmail.com>
2026-03-02 02:12:33 +0900

    feat: setup main module structure for Plan Y

--- src/GameTracer.jl
+++ src/GameTracer.jl
@@ -1,5 +1,89 @@
 module GameTracer
 
-# Write your package code here.
+using GameTheory: NormalFormGame, GAMPayoffVector
+
+# ------------------------------------------------------------------
+# Library Path
+# ------------------------------------------------------------------
+# TODO: Remove hardcoded dylib path after Phase B
+# Temporary: Using local dylib until Phase B is complete
+const libgametracer = "/tmp/gametracer_prefix/lib/libgametracer.dylib"
+
+# ------------------------------------------------------------------
+# Public API & Result Types
+# ------------------------------------------------------------------
+export ipa_solve, gnm_solve
+export IPAResult, GNMResult
+
+"""
+    IPAResult
+
+# Fields TBD
+
+"""
+struct IPAResult
+    # TBD
+end
+
+"""
+    GNMResult
+
+# Fields TBD
+
+"""
+struct GNMResult
+    # TBD
+end
+
+"""
+    ipa_solve(g::NormalFormGame) -> IPAResult
+
+# Arguements
+
+# Keyword Arguments
+
+# Returns
+
+- IPAResult:
+
+# References
+
+"""
+function ipa_solve(g::NormalFormGame)
+    
+    return IPAResult()
+end
+
+
+"""
+    gnm_solve(g::NormalFormGame) -> GNMResult
+
+# Arguements
+
+# Keyword Arguments
+
+# Returns
+
+- GNMResult: 
+
+# References
+
+"""
+function gnm_solve(g::NormalFormGame)
+    
+    return GNMResult()
+end
+
+
+# ------------------------------------------------------------------
+# Private API (C ABI wrappers)
+# ------------------------------------------------------------------
+
+
+
+
+
+
+
 
 end
